a6ef2bce9b302baf0f1b662472b8c806edea5994
Pablosudev <pablo.losu.dev@gmail.com>
2025-10-24 17:29:00 +0200

    desk

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import Mounting from "./Pages/Mounting";
 import Maintenance from "./Pages/Maintenance";
 import Service from "./Pages/Service";
 import Contact from "./Pages/Contact";
+import HomeDesktop from "./Pages/Desktop/HomeDesk";
+import LayaoutDesk from "./Pages/Layout/LayaoutDesk";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,11 +18,14 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-           <Route path="/montaje" element={<Mounting/>} />
-            <Route path="/mantenimiento" element={<Maintenance/>} />
-            <Route path="/servicios" element={<Service/>} />
-            <Route path="/contacto" element={<Contact/>} /> 
-        </Route> 
+          <Route path="/montaje" element={<Mounting />} />
+          <Route path="/mantenimiento" element={<Maintenance />} />
+          <Route path="/servicios" element={<Service />} />
+          <Route path="/contacto" element={<Contact />} />
+        </Route>
+        <Route element={<LayaoutDesk/>}>
+          <Route path="/Home" element={<HomeDesktop />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
